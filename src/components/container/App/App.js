ba546c60ecc59b0d4f90326602b38f7e24a6f1ad
zackcarlson <zackt.carlson@gmail.com>
2018-11-30 12:57:34 -0800

    Build out nav component and app component

---
+++ src/components/container/App/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import NavBar from '../NavBar/NavBar.js';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <NavBar />
+    );
+  }
+}
