68260c00188011993b3da6fb9b9b55097c5fb609
zackcarlson <zackt.carlson@gmail.com>
2018-11-30 12:56:21 -0800

    Import App

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/container/App/App.js';
 
 const appRoute = document.getElementById('app');
-ReactDOM.render(<div><h1>Hello!</h1></div>, appRoute);
+ReactDOM.render(<App/>, appRoute);
