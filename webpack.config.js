fc9384c940d7b15bce81709e1bf679dbe2d058b2
zackcarlson <zackt.carlson@gmail.com>
2018-11-30 19:24:20 -0800

    Change entry, add css rules

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,42 @@
 const path = require('path');
+const combineLoaders = require('webpack-combine-loaders');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 const publicDIR = path.join(__dirname, 'public');
 const buildDIR = path.join(__dirname, 'public/build');
 
 module.exports = {
-  entry: './src/app.js',
+  entry: { main: './src/app.js' },
   output: {
     path: path.resolve(buildDIR),
     filename: 'bundle.js'
   },
   module: {
-    rules : [{
-      loader: 'babel-loader',
-      test: /\.js$/,
-      exclude: /node_modules/
-    }]
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        loader: combineLoaders([
+          {
+            loader: 'style-loader'
+          }, {
+            loader: 'css-loader',
+            query: {
+              modules: true,
+              localIdentName: '[name]__[local]___[hash:base64:5]'
+            }
+          }
+        ]),
+      }
+    ]
   },
+  plugins: [
+    new ExtractTextPlugin('style.css')
+  ],
   mode: 'development',
   devtool: 'cheap-module-eval-source-map',
   devServer: {
