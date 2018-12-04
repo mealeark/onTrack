0e7c4c3a7c4212111cbd2aa84e4af96f322d4ca3
zackcarlson <zackt.carlson@gmail.com>
2018-12-03 17:44:59 -0800

    Import landing and tutorial

--- src/components/container/App/App.js
+++ src/components/container/App/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import NavBar from '../NavBar/NavBar.js';
+import Landing from '../../presentational/Landing/Landing.js';
+import Tutorial from '../../presentational/Tutorial/Tutorial.js';
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
         <NavBar />
+        <Landing />
+        <Tutorial />
       </div>
     );
   }
