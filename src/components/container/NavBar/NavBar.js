23b556abe67b5279d10f36a6f6f13a6507f8a338
zackcarlson <zackt.carlson@gmail.com>
2018-12-03 17:47:01 -0800

    Update syntax and import logo/styles

--- src/components/container/NavBar/NavBar.js
+++ src/components/container/NavBar/NavBar.js
@@ -6,22 +6,41 @@ class NavBar extends React.Component {
 
   render () {
     return (
-      <div className="sticky-top">
-        <nav className="navbar navbar-expand-lg navbar-light bg-light" styleName="navBar">
-          <a className="navbar-brand" href="#">Navbar</a>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
+      <div>
+        <nav 
+          className='navbar navbar-expand-lg navbar-light bg-light'
+          styleName='navBar'
+        >
+          <a className='navbar-brand' href='#'>
+            <img
+              src={require('../../../../public/images/onTrackLogo.png')}
+              alt='logo'
+              styleName='logo'
+            />
+          </a>
+          <button 
+            className='navbar-toggler'
+            type='button' 
+            data-toggle='collapse' 
+            data-target='#navbarTogglerDemo02' 
+            aria-controls='navbarTogglerDemo02'
+            aria-expanded='false'
+            aria-label='Toggle navigation'
+          >
+            <span className='navbar-toggler-icon'></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-            <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-              <li className="nav-item active">
-                <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+          <div className='collapse navbar-collapse' id='navbarTogglerDemo02'>
+            <ul className='navbar-nav mr-auto mt-2 mt-lg-0'>
+              <li className='nav-item active'>
+                <a className='nav-link' href='#'>
+                  Home <span className='sr-only'>(current)</span>
+                </a>
               </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Link</a>
+              <li className='nav-item'>
+                <a className='nav-link' href='#'>Link</a>
               </li>
-              <li className="nav-item">
-                <a className="nav-link disabled" href="#">Disabled</a>
+              <li className='nav-item'>
+                <a className='nav-link disabled' href='#'>Disabled</a>
               </li>
             </ul>
           </div>
