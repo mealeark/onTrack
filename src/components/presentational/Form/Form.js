b1dd2022e7040d3ea1f77fbc39ebd727ca4991e2
zackcarlson <zackt.carlson@gmail.com>
2018-12-05 18:12:22 -0800

    Add styling to form input

--- src/components/presentational/Form/Form.js
+++ src/components/presentational/Form/Form.js
@@ -1,15 +1,19 @@
 import React from 'react';
+import styles from './Form.css';
 import cn from 'classnames';
 
 const Form = (props) => {
+  const { inputStyle } = styles;
   return (
-   
-        <div className='form-group row justify-content-md-center'>
-          <div className='col-sm-4'>
-            <input type={props.value} className='form-control form-control-lg' id='lgFormGroupInput' placeholder={props.placeholder}/>
-          </div>
-        </div>
-  
+    <div className='form-group row justify-content-md-center'>
+      <div className='col-sm-4'>
+        <input
+          type={props.value}
+          className={cn('form-control form-control-lg', inputStyle)}
+          placeholder={props.placeholder}
+        />
+      </div>
+    </div>
   );
 };
 
