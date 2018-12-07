928622524923b7cafe63df9264a6ebd91e8c0165
zackcarlson <zackt.carlson@gmail.com>
2018-12-06 20:40:45 -0800

    Change syntax

--- src/components/presentational/Button/Button.js
+++ src/components/presentational/Button/Button.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import cn from 'classnames';
-import styles from './Button.css';
 
-const Button = (props) => {
-  const { btnStyle } = styles;
-  return (
-    <button 
-      type='button'
-      className={cn('btn btn-primary btn-lg', btnStyle, props.style)}
-    >
-      {props.buttonPhrase}
-    </button>
-  );
-};
+const Button = (props) => (
+  <button 
+    type='button'
+    className={cn('btn btn-primary btn-lg', 'btnStyle', props.style)}
+  >
+    {props.buttonPhrase}
+  </button>
+);
 
 Button.defaultProps = {
   buttonPhrase: 'Submit'
