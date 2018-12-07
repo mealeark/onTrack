d7396f27142e215ae0602ebec55a984ed9702e67
zackcarlson <zackt.carlson@gmail.com>
2018-12-06 20:42:22 -0800

    Link signup and login buttons to respective pages

--- src/components/presentational/GetStarted/GetStarted.js
+++ src/components/presentational/GetStarted/GetStarted.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import cn from 'classnames';
-import styles from './GetStarted.css';
 import Button from '../Button/Button.js';
+import { Link } from 'react-router-dom';
 
-const GetStarted = (props) => {
-  const { divStyle, signupBtn, loginBtn } = styles;
-  return (
-    <div className={cn(divStyle)}>
-      <div className='row justify-content-md-center'>
-        <div className='col-md-2 col-xs-2 col1 center-block'>
-          <Button buttonPhrase={'Signup'} style={signupBtn}/>
-        </div>
-        <div className='col-md-2 col-xs-2 col1 center-block'>
-          <Button buttonPhrase={'Login'} style={loginBtn}/>
-        </div>
-      </div>
+const GetStarted = (props) => (
+  <div className={cn('get__started__container')}>
+    <div className='row justify-content-md-center'>
+      <Link to='/sign-up' className='col-md-2 col-xs-2 col1 center-block'>
+        <Button
+          buttonPhrase={'Signup'}
+          style={'signupBtn'}
+        />
+      </Link>
+      <Link to='/login' className='col-md-2 col-xs-2 col1 center-block'>
+        <Button
+          buttonPhrase={'Login'}
+          style={'loginBtn'} 
+        />
+      </Link>
     </div>
-  );
-};
+  </div>
+);
 
 export default GetStarted;
