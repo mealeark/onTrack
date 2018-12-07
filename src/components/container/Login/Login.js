cb3f9b4d75ea31a40cbe10f3c8cdb9ea5e4f05cf
zackcarlson <zackt.carlson@gmail.com>
2018-12-06 20:38:31 -0800

    Add styling to button

--- src/components/container/Login/Login.js
+++ src/components/container/Login/Login.js
@@ -1,30 +1,39 @@
 import React from 'react';
-import styles from './Login.css';
 import cn from 'classnames';
 import Form from '../../presentational/Form/Form.js';
 import Header from '../../presentational/Header/Header.js';
 import Button from '../../presentational/Button/Button.js';
+import { Link } from 'react-router-dom';
 
 const vals = ['email', 'password'];
 
 class Login extends React.Component {
   render() {
-    const { colCentered, divStyle } = styles;
     return (
       <div>
         <div className='row justify-content-md-center'>
-          <div className={cn('col-md-2 col-md-offset-5', colCentered)}>
-            <Header title={'Login'}/>
+          <div className={cn('col-md-2 col-md-offset-5')}>
+            <Header title={'Login'} style={{textAlign: 'center'}}/>
           </div>
         </div>
-        <form className={cn(divStyle)}>
+        <form >
           { vals.map((el, i) => <Form value={el} placeholder={el} key={i}/>)}
         </form>
-        <div className={cn('row justify-content-md-center', divStyle)}>
+        <div className={cn('row justify-content-md-center')}>
           <div className='col-sm-4'>
             <Button buttonPhrase={'Login'}/>
           </div>
         </div>
+        <div className='row justify-content-md-center'>
+          <div className='col-sm-4' style={{textAlign: 'center'}}>
+          <br/>
+          <hr className='hr__text' data-content='or'></hr>
+            <Link to='/sign-up'>
+              <Button buttonPhrase={'Signup'} style={'btnStyle__alt'}/>
+            </Link>
+          </div>
+        </div>
+
       </div>
     );
   }
