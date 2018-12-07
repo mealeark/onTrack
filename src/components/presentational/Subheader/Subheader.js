cd758ecc788e4119550b7b520823cf56a6620d60
zackcarlson <zackt.carlson@gmail.com>
2018-12-06 20:48:58 -0800

    Change syntax

--- src/components/presentational/Subheader/Subheader.js
+++ src/components/presentational/Subheader/Subheader.js
@@ -1,17 +1,13 @@
 import React from 'react';
-import styles from './Subheader.css';
 import cn from 'classnames';
 
-const Subheader = (props) => {
-  const { subheader } = styles;
-  return (
-    <div>
-      <h2 className={cn(subheader, props.style)}>
-        {props.subheading}
-      </h2>
-    </div>
-  );
-};
+const Subheader = (props) => (
+  <div>
+    <h2 className={cn('subheader__style', props.style)}>
+      {props.subheading}
+    </h2>
+  </div>
+);
 
 Subheader.defaultProps = {
   subheading: 'A better way to track your job search than Excel spreadsheets.'
