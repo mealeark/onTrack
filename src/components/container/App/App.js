fb833101db3800b4681e73a256fe7d4669204586
zackcarlson <zackt.carlson@gmail.com>
2018-12-06 20:37:25 -0800

    Add switch and routes

--- src/components/container/App/App.js
+++ src/components/container/App/App.js
@@ -1,21 +1,30 @@
 import React from 'react';
 import NavBar from '../NavBar/NavBar.js';
 import Landing from '../../presentational/Landing/Landing.js';
-import Tutorial from '../../presentational/Tutorial/Tutorial.js';
-import GetStarted from '../../presentational/GetStarted/GetStarted';
 import Login from '../Login/Login.js';
 import Signup from '../Signup/Signup.js';
+import { Route, Switch } from 'react-router-dom';
 
 export default class App extends React.Component {
+  state = {
+    isLoggedIn: false,
+  }
+
   render() {
+    const { isLoggedIn } = this.state;
+    let nav = <NavBar/>;
+    if (isLoggedIn) {
+      nav = <NavBar links={['Link1', 'Link2', 'Link3']}/>
+    }
+    
     return (
       <div>
-        <NavBar />
-        <Landing />
-        <Tutorial />
-        <GetStarted />
-        <Login />
-        <Signup />
+        {nav}
+        <Switch>
+          <Route exact path='/' component={Landing}/>
+          <Route path='/login' component={Login}/>
+          <Route path='/sign-up' component={Signup}/>
+        </Switch>
       </div>
     );
   }
