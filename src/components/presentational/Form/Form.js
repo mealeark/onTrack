21d797a07c7768746baae0f1943c2db43be9ea4a
zackcarlson <zackt.carlson@gmail.com>
2018-12-06 20:41:17 -0800

    Change syntax

--- src/components/presentational/Form/Form.js
+++ src/components/presentational/Form/Form.js
@@ -1,20 +1,16 @@
 import React from 'react';
-import styles from './Form.css';
 import cn from 'classnames';
 
-const Form = (props) => {
-  const { inputStyle } = styles;
-  return (
-    <div className='form-group row justify-content-md-center'>
-      <div className='col-sm-4'>
-        <input
-          type={props.value}
-          className={cn('form-control form-control-lg', inputStyle)}
-          placeholder={props.placeholder}
-        />
-      </div>
+const Form = (props) => (
+  <div className='form-group row justify-content-md-center'>
+    <div className='col-sm-4'>
+      <input
+        type={props.value}
+        className={cn('form-control form-control-lg', 'input__style')}
+        placeholder={props.placeholder}
+      />
     </div>
-  );
-};
+  </div>
+);
 
 export default Form;
