7783f62cd414c5f67ecc811a37a86a0daac51155
zackcarlson <zackt.carlson@gmail.com>
2018-12-06 20:36:23 -0800

    Wrap app in broswerrouter

--- src/app.js
+++ src/app.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/container/App/App.js';
+import { BrowserRouter as Router } from 'react-router-dom';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+const routes = (
+  <Router>
+    <App/>
+  </Router>
+);
 
 const appRoute = document.getElementById('app');
-ReactDOM.render(<App/>, appRoute);
+ReactDOM.render(routes, appRoute);
