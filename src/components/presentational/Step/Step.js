02e92c67410199c8a663fb5ffa42beefe5797eae
zackcarlson <zackt.carlson@gmail.com>
2018-12-06 20:47:40 -0800

    Change syntax

--- src/components/presentational/Step/Step.js
+++ src/components/presentational/Step/Step.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import Header from '../Header/Header.js';
-import styles from './Step.css';
 import cn from 'classnames';
 
 
 const Step = (props) => {
   const { color } = props;
-  const { stepFont } = styles;
   const divStyle = {
     stepColor: {
       color: color,
@@ -16,12 +14,12 @@ const Step = (props) => {
   
   const { stepColor } = divStyle;
   return (
-      <div className='col-sm'>
-        <Header title={props.number + 1} style={{ ...stepColor }}/>
-        <div className={cn(stepFont)}>
-          {props.step}
-        </div>
+    <div className='col-sm'>
+      <Header title={props.number + 1} style={{ ...stepColor }}/>
+      <div className={cn('step__style')}>
+        {props.step}
       </div>
+    </div>
   );
 };
 
