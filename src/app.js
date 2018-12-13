106b15e15a044d9beae3be8a65fecaae67585a0a
zackcarlson <zackt.carlson@gmail.com>
2018-12-12 16:42:27 -0800

    Change import

--- src/app.js
+++ src/app.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/container/App/App.js';
 import { BrowserRouter as Router } from 'react-router-dom';
+import App from './components/container/App/App.js';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
+const appRoute = document.getElementById('app');
 const routes = (
   <Router>
     <App/>
   </Router>
 );
-
-const appRoute = document.getElementById('app');
 ReactDOM.render(routes, appRoute);
