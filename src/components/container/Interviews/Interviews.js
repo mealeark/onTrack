e00fa353eedd69667354d9356b32df67e0c6edeb
Mealear <mileyy.mk@gmail.com>
2019-01-03 19:38:52 -0800

    Creates container for Interviews component

---
+++ src/components/container/Interviews/Interviews.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import cn from 'classnames';
+
+class Interviews extends React.Component {
+
+  render () {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+export default Interviews;
